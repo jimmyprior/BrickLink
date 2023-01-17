7cf860d4069b6ba3ce52f5dc437eaa2b92edf587
Jimmy Prior <jimmyprior04@gmail.com>
2023-01-17 10:33:51 -0500

    setup.py yah

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bricklink",
+    version="0.1.0",
+    description= "Bricklink API",
+    author="Jimmy Prior",
+    author_email="jimmyprior04@gmail.com",
+    python_requires=">=3.9.0",
+    packages=find_packages(),
+    install_requires=[]
+)
